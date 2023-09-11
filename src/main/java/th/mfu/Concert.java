04f4871a00599c05341fc5ea7d36ab50b0e416a3
Woraphat Sriwichai <6531503071@lamduan.mfu.ac.th>
2023-09-11 18:32:39 +0700

    submit

--- src/main/java/th/mfu/Concert.java
+++ src/main/java/th/mfu/Concert.java
@@ -5,22 +5,54 @@ import java.util.Date;
 public class Concert {
 
     //TODO: add attributes
+    private int id;
+    private String title;
+    private String performer;
+    private Date date;
+    private String description;
 
     public Concert() {
     }
     public Concert(String title, String description) {
         //TODO: set attributes
+        this.title = title;
+        this.description = description;
     }
  
     public String getTitle() {
         //TODO: return title
-        return "";
+        return this.title;
     }
     public String getDescription() {
         //TODO: return description
-        return "";
+        return this.description;
     }
 
     //TODO: add getters and setters
-
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getPerformer() {
+        return performer;
+    }
+    public void setPerformer(String performer) {
+        this.performer = performer;
+    }
+    
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public Date getDate() {
+        return date;
+    }
+    public void setDate(Date date) {
+        this.date = date;
+    }
+    
 }
--- src/main/java/th/mfu/ConcertController.java
+++ src/main/java/th/mfu/ConcertController.java
@@ -20,44 +20,61 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class ConcertController {
     // TODO: create hashmap of concerts for storing data
+    private int nextId = 1;
+    private HashMap<Integer, Concert> db = new HashMap<>();
 
     //TODO: add initbinder to convert date
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
+    binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true));
+    }
 
     @GetMapping("/concerts")
     public String listConcerts(Model model) {
         // TODO: add concerts to model
+        model.addAttribute("concerts", db.values());
         // TODO: return a template to list concerts
-        return "";
+        return "list-concert";
     }
 
     @GetMapping("/add-concert")
     public String addAConcertForm(Model model) {
         // TODO: pass blank concert to a form
+        model.addAttribute("concert", new Concert());
         // TODO: return a template for concert form
-        return "";
+        return "add-concert-form";
     }
 
     @PostMapping("/concerts")
     public String saveConcert(@ModelAttribute Concert concert) {
         // TODO: add concert to list of concerts
+        int id = nextId++;
+        concert.setId(id);
+        db.put(id, concert);
         // TODO: increment nextId
         // TODO: redirect to list concerts
-        return "";
+        return "redirect:/concerts";
     }
 
     @GetMapping("/delete-concert/{id}")
     public String deleteConcert(@PathVariable int id) {
         // TODO: remove concert from list of concerts
+        Concert inst = db.getOrDefault(id, null);
+        if (inst != null) {
+           db.remove(id);
+        }
         // TODO: redirect to list concerts
-        return "";
+        return "redirect:/concerts"; 
     }
-
     
     @GetMapping("/delete-concert")
     public String removeAllConcerts() {
         //TODO: clear all employees and reset id
+        db.clear();
+        nextId = 1;
         // TODO: redirect to list concerts
-        return "";
+        return "redirect:/concerts";
     }
 
 }
